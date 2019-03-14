e0b51d1ee193c5d981ba6966297296c96395b118
rombrandon <17074290772@163.com>
2019-03-14 11:37:58 +0800

    demo init

---
+++ index.js
@@ -0,0 +1,6 @@
+import Ui from './src/Ui'
+import Hello from './src/Hello'
+export {
+  Ui,
+  Hello
+}
---
+++ src/Hello.js
@@ -0,0 +1,5 @@
+export default class Hello {
+  static say () {
+    console.log('Hello react-native-ui-tool')
+  }
+}
